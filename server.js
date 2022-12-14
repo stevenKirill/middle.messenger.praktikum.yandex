e2c8054fbbcbf432fae023e2d87958e2a9d42f4d
Kirill Pavlovskii <kirill-pavlovskij@rambler.ru>
2022-12-14 11:15:55 +0300

    fix: code review fixed

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
 });
 
 app.use(express.static(`${__dirname}/dist`));
