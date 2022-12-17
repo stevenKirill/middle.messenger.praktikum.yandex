ba170c72083f114aa66ed174a757561de8fdef15
Kirill Pavlovskii <kirill-pavlovskij@rambler.ru>
2022-12-17 17:15:06 +0300

    feat: add namespaces

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,31 @@
-console.log('hello menu');
 const a: number = 1;
 console.log(a)
+
+// a: {
+//   b: {
+//     c: {
+//       d: {
+//         e: {
+
+//         }
+//       }
+//     }
+//   }
+// }
+
+function namespace(str: string) {
+  const arr = str.split('.');
+  const startObj: {[key: string]: {}} = {};
+  let curObj = startObj;
+  for (const char of arr) {
+    curObj[char] = {}; // e: {}
+    console.log(curObj, "=> before")
+    curObj = curObj[char]; // { e: {} }
+  }
+  return startObj;
+}
+
+const namespace2 = (str: string): object =>
+  str.split(".").reduceRight((acc, key) => ({ [key]: acc }), {});
+
+console.log(namespace('a.b.c.d.e'))
