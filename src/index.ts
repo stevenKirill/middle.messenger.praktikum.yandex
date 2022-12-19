aa73615757c57dd8844d42eccdc31f769d200913
Kirill Pavlovskii <kirill-pavlovskij@rambler.ru>
2022-12-19 10:02:48 +0300

    feat: tmp add task

--- src/index.ts
+++ src/index.ts
@@ -29,3 +29,24 @@ const namespace2 = (str: string): object =>
   str.split(".").reduceRight((acc, key) => ({ [key]: acc }), {});
 
 console.log(namespace('a.b.c.d.e'))
+
+type Nullable<T> = T | null;
+
+const text: Nullable<HTMLDivElement> = document.getElementById(
+  "text"
+) as HTMLDivElement;
+const input: Nullable<HTMLInputElement> = document.getElementById(
+  "input"
+) as HTMLInputElement;
+
+if (!text || !input) {
+  throw new Error("нет полей");
+}
+
+const data = {
+  title: ""
+};
+
+Object.defineProperty(data, 'title', {});
+
+export default Nullable
