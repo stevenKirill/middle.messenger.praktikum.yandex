88b268870fa66208652eafac1df118e31f2c2f9f
Kirill Pavlovskii <kirill-pavlovskij@rambler.ru>
2023-02-12 12:22:42 +0300

    started adding webpack

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].js?version=[chunkhash:8]'
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.json']
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css?version=[chunkhash:8]',
+      ignoreOrder: true
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+      filename: path.resolve(__dirname, './build/index.html'),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(t|j)s?$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.(jpe?g|svg|png|gif|ico|eot|ttf|woff|woff2?)(\?v=\d+\.\d+\.\d+)?$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+    ]
+  },
+  devServer: {
+    port: "3001",
+    static: ["./build"],
+    open: true,
+    hot: true,
+    historyApiFallback: true,
+    client: {
+      overlay: {
+        errors: false,
+        warnings: false,
+      },
+    },
+  },
+}
